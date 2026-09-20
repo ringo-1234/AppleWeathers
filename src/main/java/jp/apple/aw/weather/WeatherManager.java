68e660d2b6ee6f7cb7867c4be10a97065ca115a8
アップルパイ <sou.120724.s@gmail.com>
2026-09-20 20:41:29 +0900

    feat:WeatherManager(WIP)

---
+++ src/main/java/jp/apple/aw/weather/WeatherManager.java
@@ -0,0 +1,7 @@
+package jp.apple.aw.weather;
+
+public class WeatherManager {
+    public static WeatherType currentWeather = WeatherType.SUNNY;
+    public static boolean doWeatherCycle = true;
+    public static int weatherTickLeft = 12000;
+}
